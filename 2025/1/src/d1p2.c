6a9ea4a23d346599fa9e0e01a8c57bd3c4addd6d
Eric Lehtonen <elehtone@student.hive.fi>
2025-12-04 01:19:00 +0200

    part 2 done

---
+++ 2025/1/src/d1p2.c
@@ -0,0 +1,56 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+
+void error_exit(void)
+{
+	perror("Exiting. Error: ");
+	exit (1);
+}
+
+//6684 is the answer
+int main (void)
+{
+	static char *file = "../d1p1_input";
+	FILE* fd = fopen(file, "r");
+	if (fd == NULL)
+		error_exit();
+
+	char line[6];
+	uint16_t zeroes = 0;
+	uint8_t position = 50;
+	int8_t sign = 1;
+
+	while (fgets(line, sizeof(line), fd))
+	{
+		(line[0] == 'R') ? (sign = 1) : (sign = -1);
+		line[0] = '+';
+		
+		int16_t rotation = atoi(line);
+		uint8_t modulo = rotation % 100;
+		int16_t offset = position + (modulo * sign);
+
+		zeroes += rotation / 100;
+
+		if (offset == 100 || offset == 0)
+		{
+			++zeroes;
+			position = 0;
+		}
+		else if (offset > 100)
+		{
+			++zeroes;
+			position = offset - 100;
+		}
+		else if (offset < 0)
+		{
+			(position == 0) ? zeroes : ++zeroes;
+			position = 100 - (offset * -1);
+		}
+		else
+			position = offset;
+	}
+
+	printf("Number of 0 stops and passes: %d\n", zeroes);
+	fclose(fd);
+}
