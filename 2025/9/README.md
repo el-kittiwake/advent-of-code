3567ef142a9f27e1e928c42412c9c1a0ea48351a
Eric Lehtonen <25428949+el-kittiwake@users.noreply.github.com>
2026-06-26 12:30:51 +0300

    Day 9. README.

---
+++ 2025/9/README.md
@@ -0,0 +1,233 @@
+# Day 9 (D)
+
+You are given a line delimited list of coordinates representing red tiles on the floor of a cinema. Straight lines between red tiles are marked by green tiles. Together they represent the perimeter of an axis aligned polygon.
+
+Example input:
+
+``` text
+7,1
+11,1
+11,7
+9,7
+9,5
+2,5
+2,3
+7,3
+```
+
+Represents this floor:
+
+``` text
+..............
+.......#...#..
+..............
+..#....#......
+..............
+..#......#....
+..............
+.........#.#..
+..............
+
+```
+
+My answers are: 4745816424 and 1351617690 for parts 1 and 2 respectively.
+
+## Part 1
+
+Find the rectangle with the largest area possible. The rectangle must have two of its opposite corners being two of the given coordinates (red tiles).
+
+The example given has `2,5` and `11,1` resulting in the largest rectangle requiring 50 tiles.
+
+### Part 1 solution
+
+The solution is the naive approach.
+
+Loop through each combination of coordinates and calculate its area, save and return the largest area found.
+
+Using two foreach loops on the parsed input was the quick and easy way to go about it in D. Initially I was not getting the correct value for the example (I was getting 36) this was because the tile count is inclusive, adding one to each side of the rectangle fixed this.
+
+My initial result with my input was 2147458464, which was too low. The value is suspiciously close to INT_MAX and that was because I was using int type for maxArea and connected variables. Leading to overflow.
+
+## Part 2
+
+Ostensibly the idea is the same as for part 1. However the largest rectangle must now fit within the perimeter of the polygon (perimeter inclusive).
+
+The given example creates the following shape into which the rectangle must fit:
+
+``` text
+..............
+.......#XXX#..
+.......X...X..
+..#XXXX#...X..
+..X........X..
+..#XXXXXX#.X..
+.........X.X..
+.........#X#..
+..............
+```
+
+This example has `9,5` and `2,3` resulting in 24 required tiles.
+
+### Part 2 solution
+
+Initially I thought to stay naive, checking for every rectangle like part 1 but adding a further check of each vertex to ensure it is not inside my rectangle. This would be a fairly massive undertaking.
+
+This was the most algorithm-heavy puzzle so far. It took me almost a week and a lot of searching for ideas. This is not something I am very familiar with, so it was education heavy. Ray-casting was my first non-naive thought, I didn't go that route because initially it seemed like the method I chose would be easier. It ultimately wasn't, but that was on me.
+
+The approach I went for consisted of marking all external cells, building a prefix sum array and finally iterating over all coordinate combinations as with part 1. After some wrestling with deep thought a coordinate compression step was added.
+
+Break coordinates into sorted, unique, vectors per axis. This reduces the ~500 input coordinate pairs down to ~250 unique values per axis after removing duplicates. (at least for my input). This only works because the polygon is axis aligned.
+
+Example input gives (`axesTuple` array):
+
+``` text
+xVals = [2, 7, 9, 11]     (compressed columns 0,1,2,3)
+yVals = [1, 3, 5, 7]      (compressed rows 0,1,2,3)
+```
+
+[Coordinate compression](https://phuongdinh1411.github.io/cses-analyses/problem_soulutions/graph_algorithms/coordinate_compression_analysis), reduces the 100,000² space down to at most 500² (the number of coordinates in the input file). Because all polygon edges fall on vertices long edges of the same horizontal or vertical position can be compressed down to one cell.
+
+Use binary search to find the position in the x/y vectors. Mark this position in the coordinateLookup array, this is the coordinate compressed array which maps to the input data via their index.
+
+Add 1 to each value to account for a zero buffer added around the grid. This is to enable the filling method used later.
+
+In the example this would look like (`coordinateLookup` array):
+
+``` text
+Vertex    ci  cj
+(7,1)      2   1
+(11,1)     4   1
+(11,7)     4   4
+(9,7)      3   4
+(9,5)      3   3
+(2,5)      1   3
+(2,3)      1   2
+(7,3)      2   2
+```
+
+Mark the perimeter of the polygon in a 2D boolean array (6x6 including padding).
+
+Each pair of input vertices makes an edge, going from first to last should make a complete closed polygon. Compression removed the implied points. Example shown below:
+
+``` text
+(7,1)→(11,1)  horizontal cj=1  mark [2][1],[3][1],[4][1]
+(11,1)→(11,7) vertical   ci=4  mark [4][1],[4][2],[4][3],[4][4]
+(11,7)→(9,7)  horizontal cj=4  mark [3][4],[4][4]
+(9,7)→(9,5)   vertical   ci=3  mark [3][3],[3][4]
+(9,5)→(2,5)   horizontal cj=3  mark [1][3],[2][3],[3][3]
+(2,5)→(2,3)   vertical   ci=1  mark [1][2],[1][3]
+(2,3)→(7,3)   horizontal cj=2  mark [1][2],[2][2]
+(7,3)→(7,1)   vertical   ci=2  mark [2][1],[2][2]
+```
+
+Example boolean array (`compressedField` array):
+
+``` text
+j\i:  0  1  2  3  4  5
+  0:  .  .  .  .  .  .
+  1:  .  .  #  #  #  .   ← y=1
+  2:  .  #  #  .  #  .   ← y=3
+  3:  .  #  #  #  #  .   ← y=5
+  4:  .  .  .  #  #  .   ← y=7
+  5:  .  .  .  .  .  .
+
+     x=  2  7  9  11
+```
+
+Now the perimeter is marked, internal cells are not. Now we need to mark either internal or external in order to calculate the prefix sums. I chose to mark external cells, cells that are outside the perimeter.
+
+For this I thought to flood fill using recursion. However I was dissuaded from attempting this and told to attempt using BFS [Breadth-first search](https://en.wikipedia.org/wiki/Breadth-first_search), another new concept to me.
+
+Starting at the top left in my case:
+
+  1. Add the first cell to the queue and mark it as true.
+  1. Loop through the queue.
+     1. Take the front item from the queue.
+     2. Check each cardinal direction (n/s/e/w) for validity (inside the grid, not part of the shape or already marked external).
+     2. If valid mark as true and add to the back of the queue.
+     2. Remove the current cell from the queue and continue looping.
+  1. End loop once the queue is empty.
+
+The example looks something like below. Left: once external cells are marked (`exterior` array), Right: the valid shape area (shown here for clarity, no array is ever made that looks exactly like this):
+
+``` text
+j\i:  0  1  2  3  4  5   |   j\i:  0  1  2  3  4  5
+  0:  E  E  E  E  E  E   |     0:  .  .  .  .  .  .
+  1:  E  E  #  #  #  E   |     1:  .  .  1  1  1  .
+  2:  E  #  #  .  #  E   |     2:  .  1  1  1  1  .
+  3:  E  #  #  #  #  E   |     3:  .  1  1  1  1  .
+  4:  E  E  E  #  #  E   |     4:  .  .  .  1  1  .
+  5:  E  E  E  E  E  E   |     5:  .  .  .  .  .  .
+
+E = exterior             |    . = exterior
+# = perimeter            |    1 = polygon (13 cells)
+. = interior
+```
+
+[Prefix sum](https://leetcopilot.dev/leetcode-pattern/prefix-sum/guide). We now know where the shape isn't and we can use this to make the prefix sum array.
+
+Iterating through the entire `exterior` array. If a cell is not marked true then its value is 1, add that value to the value of the cells immediately above and to the left of it. Subtract the value of the cell diagonally up and to the left to account for [shared cells](https://en.wikipedia.org/wiki/Inclusion%E2%80%93exclusion_principle).
+
+Once done, the top left should be 0 and the bottom right should be the total number of valid shape cells in the grid.
+
+Example (`prefixSumArray`):
+
+``` text
+j\i:  0  1  2  3  4  5
+  0:  0  0  0  0  0  0
+  1:  0  0  1  2  3  3
+  2:  0  1  3  5  7  7
+  3:  0  2  5  8 11 11
+  4:  0  2  5  9 13 13
+  5:  0  2  5  9 13 13
+```
+
+Finally comes checking and validating rectangles. The order we go through the input is the same as part 1, each combination of vertices is checked.
+
+We perform the same compressed coordinate lookup we did for marking the perimeter. This time starting by checking the relative values of each i and j coordinate to ensure that the smallest is ordered first. Inclusion-exclusion is used again because in the `prefixSumArray` (called P in the examples below) a location is the sum of everything from 0,0 to that location (P[i][j]). To find a rectangle (ci1,cj1)x(ci2,cj2) the parts that are not part of it need to be removed.
+
+Example input with largest rectangle at input (2,3)x(9,5) or (1,2) to (3,3) in the `prefixSumArray`.
+
+``` text
+sum = P[3][3] - P[0][3] - P[3][1] + P[0][1]
+    = 8 - 0 - 2 + 0
+    = 6
+
+P[3][3] = 8    everything  (0,0) to (3,3)
+P[0][3] = 0    strip above (0,0) to (0,3) : subtract.
+P[3][1] = 2    strip left  (0,0) to (3,1) : subtract.
+P[0][1] = 0    top-left    (0,0) to (0,1) : add back because it was subtracted twice.
+
+expected = (3-1+1) * (3-2+1) = 3 * 2 = 6
+```
+
+Below is the same as above, but with the original coordinates from the input:
+
+``` text
+width  = xVals[2] - xVals[0] + 1 = 9 - 2 + 1 = 8
+height = yVals[2] - yVals[1] + 1 = 5 - 3 + 1 = 3
+area   = 8 * 3 = 24
+```
+
+## Notes
+
+First time using D. I would say I am most familiar with C, so the syntax is quite familiar. The differences are mostly in the standard library's offerings rather than in how the language feels.
+
+It was very useful to be able to chain `map`, `sort` and `uniq` and use `array` to force evaluation. Major quality of life bonus right there. Lazy ranges are interesting, conceptually similar to Python generators or JavaScript promises in that nothing is computed until forced. The `!` template syntax (`to!long`, `map!(v => v[0])`) is also another oddity that I quickly grew to like.
+
+`Tuple` is quite a neat way of making a multi-return. I would normally make a struct to do that, but this wraps all the struct work up in a simple to use mechanism. Although, if I were to use D more, likely I would revert back to structs.
+
+The `rdmd` companion to the main `dmd` compiler is really useful for speeding up the run-edit-run cycle.
+
+This was the most algorithm-heavy puzzle so far. New to me were the concepts of:
+
+* Coordinate compression
+* Breadth-first search
+* Prefix sums
+
+Working through slowly and methodically worked for me. As long as I had at least a cursory understanding of what I was doing the task was completely manageable.
+
+Sources I found helpful:
+
+* Competitive Programmer's Handbook by Antti Laaksonen.
+* [Algorithms for Competitive Programming](https://cp-algorithms.com/)
---
+++ 2025/9/src/d9.d
@@ -0,0 +1,361 @@
+/*
+    Advent of code 2025, day 9, D
+
+    Resources:
+            https://tour.dlang.org/
+            https://dlang.org/spec/intro.html
+            https://dlang.org/phobos/
+            Programming in D by Ali Çehreli
+*/
+
+import std.stdio;
+import std.array;
+import std.string : chomp;
+import std.conv : to;
+import std.math.algebraic : abs;
+import std.typecons : Tuple;
+import std.algorithm : swap, sort, min, max, map, uniq;
+import std.range : assumeSorted;
+import std.container : DList;
+
+/*  ====    Read file, fill input array, convert to numbers    ====
+    try/catch to ensure file errors are handled.
+    D style is to foreach through each line of the file.
+        This handles EOF and trailing newlines internally.
+        foreach: perform the same operation to every element of a range
+            foreach (names; container_or_range)
+                names: arbitrary by programmer
+        chomp() removes trailing whitespace - result in new array
+        split() breaks each line by given character (here ,) - result in new array
+        to!long() conversion of each value into long - added to vector
+            ! template argument, call left with right
+            to!: to(T), template for type conversion. to!int, to!string, etc.
+        inputCoords grows by longPair each line
+*/
+long[][] fillArray(string fileName)
+{
+    File file;
+    try {
+        file = File(fileName, "r");
+    } catch (Exception e) {
+        writeln("Input file ", fileName, " Error: ", e.msg);
+        return null;
+    }
+
+    long[][] inputCoords;
+    foreach (line; file.byLine())
+    {
+        auto chomped = chomp(line);
+        auto stringPair = split(chomped, ',');
+        long[] longPair = [ to!long(stringPair[0]), to!long(stringPair[1]) ];
+        inputCoords ~= longPair;
+    }
+
+    return inputCoords;
+}
+
+/*     ====     Calculate Part 1 solution     ====
+    Using long due to an overflow issue with int. Part 1 at least returns
+        greater than INT_MAX.
+    Looping through each combination of coordinates.
+    Multiplication of the absolute value of the sum of each coordinate's axis.
+        +1 because tile count is inclusive: tiles from x1 to x2 = |x2−x1|+1
+*/
+long partOneCalc(const long[][] inputData)
+{
+    long maxArea = 0;
+    foreach (i; 0 .. inputData.length)
+    {
+        foreach (j; i + 1 .. inputData.length)
+        {
+            long currArea = (abs(inputData[j][0] - inputData[i][0]) + 1)
+                            * (abs(inputData[j][1] - inputData[i][1]) + 1);
+            if (currArea > maxArea)
+                maxArea = currArea;
+        }
+    }
+
+    return maxArea;
+}
+
+//  ==== Part 2 solution functions below ====
+//
+/* ====    Break coordinates into sorted, unique, vectors per axis    ====
+    Return type is Tuple. Although a simple struct would be better, I am trying
+        new things :)
+        Tuple: "these values are needed together temporarily"
+        Struct: "these values are a meaningful grouping worth naming"
+    Tuple alternates type and name. Or only types.
+        Essentially a struct under the covers, addressed like a struct.
+        tuple.name vs. tuple[index]
+    inputData.map!(v => v[E]).array.sort.uniq.array;
+        - .map: each pair (v) extract element [E]. Returns lazy range of long.
+            lazy in this context is akin to a promise (js) or generator (py)
+            v could be anything I chose. Lambda parameter.
+        - .array: force lazy value to become concrete (thunk like)
+        - .sort: sort ascending
+        - .uniq: On a sorted array, return only elements that are different to
+            previous. Lazy.
+*/
+Tuple!(long[], "xVals", long[], "yVals") getAxes(long[][] inputData)
+{
+    auto xVec = inputData.map!(v => v[0]).array.sort.uniq.array;
+    auto yVec = inputData.map!(v => v[1]).array.sort.uniq.array;
+
+    return Tuple!(long[], "xVals", long[], "yVals")(xVec, yVec);
+}
+
+/*  ====  Build a parallel lookup table of compressed indices  ====
+    Compressed space of 500 x 500. Actual space over 100000^2.
+    For each vertex in inputData, find its position in xVals/yVals vectors via
+        binary search (assumeSorted + lowerBound). Plus 1 for padding offset.
+    For each compressed coordinates' x/y values find the range of elements less
+        than the value. 
+        .lowerBound(<value>) returns all elements less than <value>.
+        .length counts them, giving the index of <value> in the sorted array.
+        Plus 1 to account for the zero buffer around the whole field.
+    Returns a parallel array: coordinateLookup is the compressed (x,y)
+        index pair for inputData.
+    Avoids repeating binary searches later.
+*/
+ulong[][] inputToCompressed(long[][] inputData,
+                            Tuple!(long[], "xVals", long[], "yVals") axesTuple)
+{
+    ulong[][] coordinateLookup = new ulong[][](inputData.length, 2);
+
+    foreach (vertex; 0 .. inputData.length)
+    {
+        auto i1 = inputData[vertex];
+
+        ulong ci1 = assumeSorted(axesTuple.xVals).lowerBound(i1[0]).length + 1;
+        ulong cj1 = assumeSorted(axesTuple.yVals).lowerBound(i1[1]).length + 1;
+
+        coordinateLookup[vertex] = [ci1, cj1];
+    }
+
+    return coordinateLookup;
+}
+
+/*  ====  Mark perimeter cells on compressed coordinate array  ====
+    Data in input file traces a shape, in order, vertex by vertex.
+    bool[][]: dynamic is required as the x and y lengths are not known until runtime.
+        new: allocates memory, but does not need delete/free in D.
+    foreach vertex in the coordinateLookup table
+        collect 2 adjacent vertices.
+            index vertex 2 to module of total number of vertexes in order to be
+                able to wrap around to the start of the list.
+    If the x (i) indexes are the same these vertices make a vertical edge.
+        Vice versa for horizontal edges and y (j) indexes.
+        Fill the cells from lowest to highest compressed index with `true`.
+*/
+bool[][] markPerim(Tuple!(long[], "xVals", long[], "yVals") axesTuple,
+                   ulong[][] coordinateLookup)
+{
+    bool[][] compressedField = new bool[][](axesTuple.xVals.length + 2, axesTuple.yVals.length + 2);
+
+    foreach (vertex; 0 .. coordinateLookup.length)
+    {
+        auto c1 = coordinateLookup[vertex];
+        auto c2 = coordinateLookup[(vertex + 1) % coordinateLookup.length];
+
+        if (c1[0] == c2[0])  // vertical edge
+            foreach (j; min(c1[1],c2[1]) .. max(c1[1],c2[1]) + 1)
+                compressedField[c1[0]][j] = true;
+        else             // horizontal edge
+            foreach (i; min(c1[0],c2[0]) .. max(c1[0],c2[0]) + 1)
+                compressedField[i][c1[1]] = true;
+    }
+
+    return compressedField;
+}
+
+/*  ====    Check if a given cell is within the bounds of the field    ====
+    Helper for floodFill.
+*/
+bool inBounds(long iNext, long jNext, long iMax, long jMax)
+{
+    return iNext >= 0 && jNext >= 0 && iNext < iMax && jNext < jMax;
+}
+
+/*  ====   Fill all cells external to the shape with true   ====
+    DList: doubly linked list, 2 longs (coords). Queue of coordinates to check.
+        insertBack(<values>): inserts <values> at the back
+        cast(<type>): casts values to <type>
+    exterior: an empty bool array the size of compressedField
+    cardinals: an array of offsets, used to check neighbour cells for validity.
+    while() there are some cells to check, check each of 4 neighbours (n/s/e/w):
+        .front: fetches the current front value from the DList.
+        if: inside grid and both compressedField and exterior are false
+            set exterior to true and insertBack current neighbour cell
+        .removeFront: removes the front value of the DList before looping
+    return the array of exterior cells once queue is empty.
+*/
+bool[][] floodFill(bool[][] compressedField)
+{
+    DList!(long[2]) queue;
+    queue.insertBack(cast(long[2])[0, 0]);
+
+    immutable long yMax = compressedField[0].length;
+    immutable long xMax = compressedField.length;
+    bool[][] exterior = new bool[][](xMax, yMax);
+    exterior[0][0] = true;
+
+    immutable long[2][4] cardinals = [[0,1],[0,-1],[1,0],[-1,0]];
+
+    while (!queue.empty)
+    {
+        auto currCell = queue.front;
+        foreach (c; cardinals)
+        {
+            long ni = currCell[0] + c[0];
+            long nj = currCell[1] + c[1];
+            if (inBounds(ni, nj, xMax, yMax) && !compressedField[ni][nj] && !exterior[ni][nj])
+            {
+                exterior[ni][nj] = true;
+                queue.insertBack(cast(long[2])[ni, nj]);
+            }
+        }
+        queue.removeFront();
+    }
+
+    return exterior;
+}
+
+/*  ====   Create prefix sum array for given compressed coordinates   ====
+    Sums all non-exterior cells (marked false) starting from the top left.
+        Results in a cumulative count of all valid cells in the field.
+    foreach through the whole field
+        To the current cell value (1 if not exterior) add the values of the 
+            upper and left neighbours. Subtract the diagonal to account for
+            shared cells above and to the left.
+            https://en.wikipedia.org/wiki/Inclusion%E2%80%93exclusion_principle
+*/
+int[][] prefixSum(bool[][] exterior)
+{
+    long yMax = exterior[0].length;
+    long xMax = exterior.length;
+    int[][] prefixSumArray = new int[][](xMax, yMax);
+
+    foreach (i; 0 .. exterior.length)
+    {
+        foreach (j; 0 .. exterior[0].length)
+        {
+            int val = !exterior[i][j] ? 1 : 0;
+            int above = (i > 0) ? prefixSumArray[i-1][j] : 0;
+            int left  = (j > 0) ? prefixSumArray[i][j-1] : 0;
+            int diag  = (i > 0 && j > 0) ? prefixSumArray[i-1][j-1] : 0;
+            prefixSumArray[i][j] = val + above + left - diag;
+        }
+    }
+
+    return prefixSumArray;
+}
+
+/*  ====  Query prefix sum for rectangle validity  ====
+    Inclusion-exclusion over the prefix sum array to count valid cells in the
+        rectangle (ci1,cj1)->(ci2,cj2).
+    Returns true if every cell is valid (sum equals total cell count), meaning
+        the rectangle fits inside the polygon.
+*/
+bool findPrefixSum(int[][] prefixSumArray,
+                   ulong ci1, ulong ci2, ulong cj1, ulong cj2)
+{
+    long sum = prefixSumArray[ci2][cj2] - prefixSumArray[ci1 - 1][cj2]
+          - prefixSumArray[ci2][cj1 - 1] + prefixSumArray[ci1 - 1][cj1 - 1];
+
+    long expected = (ci2 - ci1 + 1) * (cj2 - cj1 + 1);
+
+    return sum == expected;
+}
+
+/*  ====    Find possible rectangles and check for validity   ====
+    Iterate over all vertex pairs. For each pair, retrieve compressed indices,
+        normalise order, query prefix sum for validity, then calculate area
+        in original coordinates using xVals/yVals. Replace maxArea if larger
+        than current maxArea.
+        +1 on each axis because tile counts are inclusive.
+*/
+long findRectangle(long[][] inputData,
+                   Tuple!(long[], "xVals", long[], "yVals") axesTuple,
+                   ulong[][] coordinateLookup,
+                   int[][] prefixSumArray)
+{
+    long maxArea = 0;
+
+    foreach (i; 0 .. inputData.length)
+    {
+        foreach (j; i + 1 .. inputData.length)
+        {
+            ulong ci1 = coordinateLookup[i][0];
+            ulong cj1 = coordinateLookup[i][1];
+            ulong ci2 = coordinateLookup[j][0];
+            ulong cj2 = coordinateLookup[j][1];
+
+            if (ci1 > ci2) swap(ci1, ci2);
+            if (cj1 > cj2) swap(cj1, cj2);
+
+            if (!findPrefixSum(prefixSumArray, ci1, ci2, cj1, cj2))
+                continue ;
+
+            long width = axesTuple.xVals[ci2 - 1] - axesTuple.xVals[ci1 - 1] + 1;
+            long height = axesTuple.yVals[cj2 - 1] - axesTuple.yVals[cj1 - 1] + 1;
+            long currArea = width * height;
+
+            if (currArea > maxArea)
+                maxArea = currArea;
+        }
+    }
+
+    return maxArea;
+}
+
+/*     ====     Calculate Part 2 solution     ====
+    1. Collect all unique coords into sorted individual vectors for x and y.
+        - xVals and yVals.
+    2. Compressed cell (ci,cj) represents original tiles (coordinateLookup)
+        - x∈[xs[i], xs[i+1]-1], y∈[ys[j], ys[j+1]-1]
+    3. Trace polygon edges by marking perimeter cells in compressed space.
+        - compressedField
+    4. Flood fill exterior cells from a known-outside corner, added buffer.
+        - Buffer is +1 around the whole field.
+        - Remaining cells = interior (perimeter + interior = "valid")
+        - exterior
+    6. Calculate 2D prefix sum over valid cells.
+        - Sum of all cells accumulated from top left to bottom right.
+    7. Same combination pair loop as for Part 1:
+        - Find compressed indices for each vertex pair.
+        - Prefix sum query to check rectangle validity.
+        - If valid, compute original-space area with part 1.
+*/
+long partTwoCalc(long[][] inputData)
+{
+    auto axesTuple = getAxes(inputData);
+    auto coordinateLookup = inputToCompressed(inputData, axesTuple);
+    auto compressedField = markPerim(axesTuple, coordinateLookup);
+    auto exterior = floodFill(compressedField);
+    auto prefixSumArray = prefixSum(exterior);
+    auto maxArea = findRectangle(inputData, axesTuple, coordinateLookup, prefixSumArray);
+    return maxArea;
+}
+
+/*  ====    MAIN    ====
+    string type is immutable(char)[]  internally
+        immutable: compiler guarantees nobody has a mutable reference;
+                any attempted modification is a compile error.
+        const:     compiler won't let you modify it via this reference,
+                but can be modified by a mutable reference to the same data.
+*/
+void main()
+{
+    immutable string fileName = "../d9_input";
+
+    long[][] inputData = fillArray(fileName);
+    if (inputData is null)
+        return;
+
+    long partOne = partOneCalc(inputData);
+    writeln("Part 1: ", partOne);
+
+    long partTwo = partTwoCalc(inputData);
+    writeln("Part 2: ", partTwo);
+}
