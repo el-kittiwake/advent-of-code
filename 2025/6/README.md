c2a79e851c0536536bf8328f0b1851bdc247fa2c
Eric Lehtonen <25428949+el-kittiwake@users.noreply.github.com>
2026-07-10 08:47:01 +0300

    Day 6 more tidy and README cleanup.

--- 2025/6/README.md
+++ 2025/6/README.md
@@ -1,17 +1,37 @@
 # Day 6 (Julia)
 
-You are given a grid of numbers with an operator row at the bottom. Each column is a separate arithmetic problem, with the operator at the bottom determining whether to sum or multiply the numbers above it.
+I am given a grid of numbers with an operator row at the bottom. Each column is a separate arithmetic problem, with the operator at the bottom determining whether to sum or multiply the numbers above it. There are only `*` and `+` operators.
 
 Example input:
 
 ``` text
-123 328  51 64 
- 45 64  387 23 
-  6 98  215 314
-*   +   *   + 
+44 45 28  951
+75 54 224 56 
+34 72 429 722
++  *  +   +  
 ```
 
+My input has 4 rows of operands and a single row of operators and each row is 3700+ characters long.
+
 My answers are: 5784380717354 and 7996218225744 for parts 1 and 2 respectively.
+
+## Input handling
+
+The input is opened and read in a do block. Julia does not always need a explicit return as the implicit return mechanism is to return the last evaluated expression in a function.
+
+Once end-of-file is reached, `read()` returns the data it read, this propagates through the `do` block's return, to the `open()`'s return and finally is returned by the `readFile()` function to the caller.
+
+This string has its empty lines removed using the `filter()` function, this operates on the fed from the `split()` function which is used to split the string by newlines. This creates a vector of string substrings. split() always returns strings as substrings.
+
+>A SubString in Julia is a struct containing a reference to the original string, an offset to the beginning of the substring, and the length of the substring. Analogous to C++'s `std::string_view`.
+
+The string is checked to ensure all lines are of equal length (important for part 2). It is now ready for part 2. However part 1 needs this data broken up a little more.
+
+splitString = `["44 45 28  951", "75 54 224 56 ", "34 72 429 722", "+  *  +   +  "]`
+
+Each line is collected into a vector using a comprehension [ ... ]. Within this a whitespace directed `split()` is called on the result of `strip()`. `strip()` defaults to stripping whitespace. The result is a vector of vectors of string substrings.
+
+rows = `[["44", "45", "28", "951"], ["75", "54", "224", "56"], ["34", "72", "429", "722"], ["+", "*", "+", "+"]]`
 
 ## Part 1
 
@@ -19,18 +39,45 @@ The solution is the sum of all column results, processed left to right.
 
 ### Part 1 solution
 
-I convert the input file into a matrix, then iterate columnwise. The bottom row determines the operator. I convert each of the columnar rows into numbers and apply `prod()` or `sum()` functions depending on the final row's operator. I add each column's result to a running total.
+I iterate columnwise. The bottom row determines the operator. I convert each of the columnar rows into numbers and apply `prod()` or `sum()` functions depending on the final row's operator. I add each column's result to a running total.
 
 ## Part 2
 
 Each numbers' digits are now read top to bottom. Each column within a block (these blocks are the same as in part 1) is one operand, with digits running top to bottom from most to least significant. The column with the operator in the last row is the leftmost column of the block. Blocks are separated by an aligned column of spaces, everything reads right to left.
 
+Taking the example from the top of the page:
+
+``` text
+blocks:
+1      2      3       4
+┌─┐    ┌─┐    ┌──┐    ┌──┐
+44│    45│    28 │    951│
+75│    54│    224│    56 │
+34│    72│    429│    722│
++ │    * │    +  │    +  │
+```
+
+1. Block 4: 12 + 562 + 957 = 1531
+1. Block 3: 49 + 822 + 224 = 1095
+1. Block 2: 542 * 457 = 247694
+1. Block 1: 454 + 473 = 927
+
+Total: 251247
+
 ### Part 2 solution
 
-Working on the unprocessed input file contents rather than a parsed matrix, I run right to left across the columns. Each column's digits are read vertically and concatenated to form a number, each number is added to a vector. When the operator is met it marks the leftmost column of the current block. I then apply the operation to the vector of numbers and add the result to the running total.
+Working on the split but otherwise unprocessed input file (splitString), I loop right to left across the columns, helped by `reverse()`.
+
+Each column is looped through top to bottom, saving the individual digits in order to a variable (`newNum`). If newNum has a value then push that value as an integer to a vector (`currentSet`). The operator is then checked and `prod()` or `sum()` applied to currentSet, this result is added to the running total.
+
+Finally, newNum is checked for emptiness, if found empty then we were at a block separating column of spaces. If so, currentSet is emptied and the loop continues.
 
 ## Notes
+
+This is one of those puzzles that is trivially easy to understand in wetware, but causes trouble getting a computer to do it. Especially while learning the basics of a new language at the same time.
 
 Part 2 required not only a different approach to part 1, it also required a lot more time. Using the operator row to identify block boundaries and adding the parsed numbers as we went along was the key here.
 
 First time using Julia, not super enjoyable in this format. I feel that this language needs more time in order to truly get to appreciate its quirks. Lazy iterators and the concept of broadcasting are interesting. Using Unicode operators like `∈` potentially make for clean reading code. I can see it being well loved by people who work with large sets of numbers a lot.
+
+My initial solution for part 2 was a little longer. Using `all()`, `any()` and `join()` each fed a lazy iterator/generator expression. That version was looping through the column three times. The current version only once. Initially I continued using `all()` and `any()`, this resulted in them only operating on one index's character, not the whole column. It took me a while to figure out why the result was wrong.
--- 2025/6/src/d6.jl
+++ 2025/6/src/d6.jl
@@ -13,7 +13,9 @@
 # Function declarations
 # =====================
 #=
-	Open and read an input filename
+	Open input filename in read mode (default anyway).
+	Read the file until EOF then exit the do loop.
+	String is returned. No explicit return, value propagates back from do.
 	https://www.geeksforgeeks.org/julia/opening-and-reading-a-file-in-julia/
 =#
 function readFile(filename::String)::String
@@ -26,37 +28,30 @@ end
 	Part 1
 	- Calculate size of input data. length(<dimension>)
 	- Initialise a counter: <var> = zero(<type>)
+		Ensures the correct width of integer.
+		https://docs.julialang.org/en/v1/base/numbers/#Base.zero
 	- Loop until reaching the end of the array.
 	- Test for * or + final row (only these are possible in input files)
 	- Calculate result using:
 		prod() - multiply whatever is given as parameter
 		parse.() - parse values in the given range to UInt64
 			broadcasting(.) performs the parse on all values
+		https://docs.julialang.org/en/v1/base/numbers/#Base.parse
 	- Add current result to count and return once end reached
 =#
 function partOne(inputData::Vector{Vector{SubString{String}}})::UInt64
-	# length(<collection>): number of elements in <collection>
-	# https://docs.julialang.org/en/v1/base/collections/#Base.length
-	width = length(inputData[1])
 	height = length(inputData)
+	width = length(inputData[1])
 
-	# var = zero(type) is not necessary, but good Julia practise.
-	#	Ensures the correct width of integer.
-	# https://docs.julialang.org/en/v1/base/numbers/#Base.zero
+	# Counter initialised with type.
 	count = zero(UInt64)
 
-	for i in 1:width
-		if inputData[height][i] == "*"
-			# prod() - Multiply the results of calling the function f on each
-			# 	element of an array over the given dimensions.
-			# sum() - same for summation
-			# https://docs.julialang.org/en/v1/base/collections/#Base.prod
-			# https://docs.julialang.org/en/v1/base/collections/#Base.sum
-			# parse() - Parse a string as a number of given format
-			# https://docs.julialang.org/en/v1/base/numbers/#Base.parse
-			result = prod(parse.(UInt64, [inputData[r][i] for r in 1:height-1]))
+	# Loop over the whole length of a line.
+	for c in 1:width
+		if inputData[height][c] == "*"
+			result = prod(parse.(UInt64, [inputData[r][c] for r in 1:height-1]))
 		else
-			result = sum(parse.(UInt64, [inputData[r][i] for r in 1:height-1]))
+			result = sum(parse.(UInt64, [inputData[r][c] for r in 1:height-1]))
 		end
 
 		count += result
@@ -69,58 +64,45 @@ end
 	Part 2
 	- Calculate size of matrix.
 		size(<array>, <dimension>) - for height
-		length(<string>) - for width, due to it being the string we will work on
 	- Initialise a vector for parsed numbers and total counter
 	- Loop through the width of the string backwards reverse()
-		If the whole column contains a digit it is a member number of the set
-			Vertically concatenate the individual digits and add the result to a vector
-		If the column ends with an operator it is the last column and digit for that set
-			Perform the desired operation on the set prod() or sum()
-			Add result to current total count
-		If the column contains all spaces it is a separator
-			Empty the current set vector empty!()
+		&& (short circuiting and): if left then do right.
+		* is the string concatenation operator.
+		|| (short circuiting or): if not left then right
+		isempty(<collection>): true if <collection> has no elements
+		push!(<destination>, <values>): insert <values> at end of <destination>
+		∈: "is in" operator
+		empty!(<collection>): clear <collection>
 	- Return end result
 =#
 function partTwo(splitString::Vector{SubString{String}})::UInt64
-	width = length(splitString[1])
 	height = size(splitString, 1)
+	width = length(splitString[1])
 
-	# Matrix declared with its type only.
+	# Matrix declared with its type only. Counter declared.
 	currentSet = UInt64[]
 	count = zero(UInt64)
 
+	# Loop backwards over the whole length of a line.
 	for c in reverse(1:width)
-		# any() - Determine whether a condition returns true for any elements
-		#		  along the given dimensions of an array.
-		# join() - Concatenates given values. Within the conditions given
-		# push!() - Adds to collection the given values (parsed from string to num)
-		# https://docs.julialang.org/en/v1/base/collections/#Base.push!
-		if any(isdigit, splitString[i][c] for i in 1:height-1)
-			newNum = join("$(splitString[i][c])" for i in 1:height-1 if isdigit(splitString[i][c]))
-			push!(currentSet, parse(UInt64 ,newNum))
+		# Add digit character to whole number string
+		newNum = ""
+		for i in 1:height-1
+			ch = splitString[i][c]
+			isdigit(ch) && (newNum *= ch)
 		end
 
-		# Checks the last row at loop position for either * or +.
-		# ∈ - "element of" x ∈ collection: true if x is in collection. Same as in operator.
-		# https://www.juliabloggers.com/what-is-%E2%88%88-in-julia/
-		if splitString[height][c] ∈ ('*', '+')
-			if splitString[height][c] == '*'
-				result = prod(currentSet)
-			else
-				result = sum(currentSet)
-			end
+		# Add new number to currentSet if it exists
+		isempty(newNum) || push!(currentSet, parse(UInt64, newNum))
 
+		# Product or sum currentSet depending on last row value
+		if splitString[height][c] ∈ ('*', '+')
+			result = splitString[height][c] == '*' ? prod(currentSet) : sum(currentSet)
 			count += result
 		end
 
-		# Like any() but only returns true if all of a given collection matches the
-		# condition.
-		# empty!() - clears the given array. Prevents reallocation
-		# https://docs.julialang.org/en/v1/base/collections/#Base.empty!
-		if all(isspace, splitString[i][c] for i in 1:height-1)
-			empty!(currentSet)
-		end
-		
+		# If column is all space, clear currentSet and move on
+		isempty(newNum) && empty!(currentSet)
 	end
 
 	return count
@@ -129,7 +111,6 @@ end
 # =====================
 # "Main"
 # =====================
-
 #=
 	Read input data
 =#
@@ -141,10 +122,11 @@ fileContents = readFile(fileLocation)
 	filter(<criteria>, <content>) - filter empty lines out
 	split(<string>, <delim>) - split strings by delim into array
 	allequal(<content>) - check all lines are equal length
-	|| @warn - Fail to warning the user about unequal line lengths
+	|| (short circuiting or)
+		@warn - If failure: warning message to user about unequal line lengths.
 =#
 splitString = filter(!isempty, split(fileContents, '\n'))
-allequal(length.(splitString)) || @warn "Input file line widths not equal. Part 2 may fail."
+allequal(length.(splitString)) || @warn "Input file line widths not equal. Will likely fail."
 
 #=
 	Part 1
